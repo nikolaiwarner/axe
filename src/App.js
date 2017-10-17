776b59937ca8350e01f6f05eb7bbdad0a01bf6f2
Nick Warner <nickwarner@gmail.com>
2017-10-17 01:11:47 -0400

    working

--- src/App.js
+++ src/App.js
@@ -16,6 +16,24 @@ const randomPhrase = (amount = 5) => {
     return words[Math.floor(Math.random() * words.length)]
   }).join(' ')
 }
+const randomColor = () => {
+  return `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`
+}
+const randomFont = () => {
+  let fonts = [
+    'https://raw.githubusercontent.com/etiennepinchon/aframe-fonts/master/fonts/amaticsc/AmaticSC-Regular.json',
+    'https://raw.githubusercontent.com/etiennepinchon/aframe-fonts/master/fonts/carterone/CarterOne.json',
+    'https://raw.githubusercontent.com/etiennepinchon/aframe-fonts/master/fonts/corben/Corben-Bold.json',
+    'https://raw.githubusercontent.com/etiennepinchon/aframe-fonts/master/fonts/fontdinerswanky/FontdinerSwanky-Regular.json',
+    'https://raw.githubusercontent.com/etiennepinchon/aframe-fonts/master/fonts/hanuman/Hanuman-Regular.json',
+    'https://raw.githubusercontent.com/etiennepinchon/aframe-fonts/master/fonts/matesc/MateSC-Regular.json',
+    'https://raw.githubusercontent.com/etiennepinchon/aframe-fonts/master/fonts/monoton/Monoton-Regular.json',
+    'https://raw.githubusercontent.com/etiennepinchon/aframe-fonts/master/fonts/nosifer/Nosifer-Regular.json',
+    'https://raw.githubusercontent.com/etiennepinchon/aframe-fonts/master/fonts/reeniebeanie/ReenieBeanie.json',
+    'https://raw.githubusercontent.com/etiennepinchon/aframe-fonts/master/fonts/sigmarone/SigmarOne-Regular.json'
+  ]
+  return fonts[randomInt(0, (fonts.length - 1))]
+}
 
 const SpeechToText = new Artyom()
 
@@ -23,7 +41,9 @@ class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
+      backgroundColor: 'rgb(0, 255, 0)',
       currentCamera: 1,
+      currentFont: randomFont(),
       hitting: false,
       swinging: false,
       text: 'axe me anything',
@@ -35,16 +55,14 @@ class App extends Component {
   componentDidMount () {
 
     // setInterval(() => {
-    //   let currentCamera = this.state.currentCamera + 1
-    //   if (currentCamera > 4) {
-    //     currentCamera = 1
-    //   }
     //   this.setState({
-    //     currentCamera
+    //     currentCamera: 2
     //   })
-    // }, 2000)
+    // }, 100)
 
-    this.fakeTalking()
+    // this.fakeTalking()
+
+    // this.backgroundColor()
 
     this.startVideo()
     this.startDictation()
@@ -58,44 +76,47 @@ class App extends Component {
     let text1 = text
     let text2 = ''
     if (text.length > 1) {
-      let middle = Math.floor(text.length / 2)
-      let numberOfSpaces = (text.length - 1) - middle
+      // let middle = Math.floor(text.length / 2)
+      // let numberOfSpaces = (text.length - 1) - middle
+      //
+      // let spaces = [...Array(numberOfSpaces)].map(() => '_').join('')
+      // text1 = text.slice(0, middle)
+      // text1 = text1 + spaces
+      // text2 = text.slice(middle)
+      // text2 = spaces + text2
 
-      let spaces = [...Array(numberOfSpaces)].map(() => '_').join('')
-      text1 = text.slice(0, middle)
-      text1 = text1 + spaces
-      text2 = text.slice(middle)
-      text2 = spaces + text2
-
-      console.log(text1)
-      console.log(text2)
+      // console.log(text1)
+      // console.log(text2)
     }
     if (!this.state.swinging && !this.state.hitting) {
+      this.resetAllVideos()
       this.setState({
+        currentFont: randomFont(),
+        currentFontSize: randomInt(15, 24),
+        currentColor: randomColor(),
         text,
         text1,
         text2,
         swinging: true
       }, () => {
-        let swingVideo = document.querySelector('#swing1')
-        swingVideo.currentTime = 0
-        swingVideo.play()
-        let hitDelay = 2000
+        let hitDelay = 2500
+        // Show hit camera
         setTimeout(() => {
           this.setState({currentCamera: randomInt(3, 4)})
         }, randomInt(0, hitDelay))
+        // Hit the target
         setTimeout(() => {
-          this.setState({hitting: true})
-          setTimeout(() => {
-            this.setState({hitting: false})
-          }, randomInt(300, 1500))
+          this.setState({hitting: true, backgroundColor: randomColor()})
+          // setTimeout(() => {
+          //   this.setState({hitting: false})
+          // }, randomInt(1000, 5000))
           // TODO: animate explosion ......
 
           // Possibly show alt camera angle
           if (randomBool()) {
             setTimeout(() => {
               this.setState({currentCamera: randomInt(3, 4)})
-            }, randomInt(0, 1000))
+            }, randomInt(0, 3000))
           }
 
           // Resume loop
@@ -103,17 +124,32 @@ class App extends Component {
             this.setState({swinging: false, hitting: false}, () => {
               this.startQuietTimeline()
             })
-          }, randomInt(1000, 3000))
+          }, randomInt(1000, 5000))
         }, hitDelay)
       })
     }
+  }
+
+  backgroundColor () {
+    setTimeout(() => {
+      this.setState({backgroundColor: randomColor()})
+      this.backgroundColor()
+    }, randomInt(5000, 20000))
   }
 
   fakeTalking () {
     setTimeout(() => {
       this.onSomeoneSaysSomething(randomPhrase())
       this.fakeTalking()
-    }, randomInt(2000, 10000))
+    }, randomInt(5000, 20000))
+  }
+
+  resetAllVideos () {
+    let videos = document.querySelectorAll('.video')
+    videos.forEach((video) => {
+      video.currentTime = 100
+      video.play()
+    })
   }
 
   startVideo () {
@@ -121,7 +157,7 @@ class App extends Component {
   }
 
   startDictation () {
-    SpeechToText.say('axe')
+    // SpeechToText.say('axe')
     window.SpeechToTextDictation = SpeechToText.newDictation({
       continuous: true,
       onResult: this.onSomeoneSaysSomething.bind(this),
@@ -135,7 +171,10 @@ class App extends Component {
       this.setState({currentCamera: 1})
       setTimeout(() => {
         if (!this.state.swinging && !this.state.hitting) {
-          this.setState({currentCamera: 2})
+          this.setState({
+            currentCamera: 2,
+            cameraTwoVideo: `#wide${randomInt(1, 4)}`
+          })
           setTimeout(() => {
             this.startQuietTimeline()
           }, randomInt(4000, 8000))
@@ -153,28 +192,39 @@ class App extends Component {
           className='scene'
         >
           <a-assets>
-            <a-asset-item id='optimerBoldFont' src='https://rawgit.com/mrdoob/three.js/dev/examples/fonts/optimer_bold.typeface.json' />
+            <a-asset-item id='font1' src='https://raw.githubusercontent.com/etiennepinchon/aframe-fonts/master/fonts/amaticsc/AmaticSC-Regular.json' />
+            <a-asset-item id='font2' src='https://raw.githubusercontent.com/etiennepinchon/aframe-fonts/master/fonts/carterone/CarterOne.json' />
+            <a-asset-item id='font3' src='https://raw.githubusercontent.com/etiennepinchon/aframe-fonts/master/fonts/corben/Corben-Bold.json' />
+            <a-asset-item id='font4' src='https://raw.githubusercontent.com/etiennepinchon/aframe-fonts/master/fonts/fontdinerswanky/FontdinerSwanky-Regular.json' />
+            <a-asset-item id='font5' src='https://raw.githubusercontent.com/etiennepinchon/aframe-fonts/master/fonts/hanuman/Hanuman-Regular.json' />
+            <a-asset-item id='font6' src='https://raw.githubusercontent.com/etiennepinchon/aframe-fonts/master/fonts/matesc/MateSC-Regular.json' />
+            <a-asset-item id='font7' src='https://raw.githubusercontent.com/etiennepinchon/aframe-fonts/master/fonts/monoton/Monoton-Regular.json' />
+            <a-asset-item id='font8' src='https://raw.githubusercontent.com/etiennepinchon/aframe-fonts/master/fonts/nosifer/Nosifer-Regular.json' />
+            <a-asset-item id='font9' src='https://raw.githubusercontent.com/etiennepinchon/aframe-fonts/master/fonts/reeniebeanie/ReenieBeanie.json' />
+            <a-asset-item id='font10' src='https://raw.githubusercontent.com/etiennepinchon/aframe-fonts/master/fonts/sigmarone/SigmarOne-Regular.json' />
 
-            <img id='closeup1' src='video/temp-face1.jpg' />
-            <img id='wide1' src='video/temp-face2.jpg' />
-
-            <video id='swing1' src='video/temp-swing2.mp4' autoPlay loop muted playsInline className={'video'} />
+            <video id='close1' src='video/close1.mp4' autoPlay loop muted playsInline className={'video'} />
+            <video id='swing1' src='video/swing1.mp4' autoPlay loop muted playsInline className={'video'} />
+            <video id='wide1' src='video/wide1.mp4' autoPlay loop muted playsInline className={'video'} />
+            <video id='wide2' src='video/wide2.mp4' autoPlay loop muted playsInline className={'video'} />
+            <video id='wide3' src='video/wide3.mp4' autoPlay loop muted playsInline className={'video'} />
+            <video id='wide4' src='video/wide4.mp4' autoPlay loop muted playsInline className={'video'} />
           </a-assets>
-          <a-sky color='#00FF00' />
+          <a-sky color={this.state.backgroundColor} />
 
           <Entity position={{x: -100, y: 0, z: 0}} rotation={{x: 0, y: 90, z: 0}}>
-            <Entity camera={{active: (this.state.currentCamera === 1)}} position={{x: 2, y: 2, z: 0}} />
+            <Entity camera={{active: (this.state.currentCamera === 1)}} position={{x: 1.5, y: 3, z: -2.3}} />
             <Entity
-              name={'closeup1'}
+              name={'close1'}
               geometry={{primitive: 'plane', width: 4, height: 8}}
               position={{x: 0, y: 2.5, z: -4}}
               rotation={{x: 0, y: 0, z: 0}}
-              material={{src: '#closeup1'}}
+              material={{shader: 'chromakey', src: '#close1', color: '0 1 0'}}
             />
             {this.state.hitting &&
               <Entity
                 position={{x: 0, y: -5, z: 0}}
-                particle-system={{color: '#FF00FF'}}
+                particle-system
                 maxAge={2}
               />
             }
@@ -182,46 +232,75 @@ class App extends Component {
 
           <Entity position={{x: 200, y: 0, z: 0}} rotation={{x: 0, y: 180, z: 0}}>
             <Entity camera={{active: (this.state.currentCamera === 2)}} position={{x: 2, y: 2, z: 0}} />
-            <Entity
-              id={'wide1'}
-              name={'wide1'}
-              geometry={{primitive: 'plane', width: 4, height: 8}}
-              position={{x: 0, y: 2.5, z: -4}}
-              rotation={{x: 0, y: 0, z: 0}}
-              material={{src: '#wide1'}}
-            />
+            {this.state.cameraTwoVideo === '#wide1' &&
+              <Entity
+                id={'wide1'}
+                name={'wide1'}
+                geometry={{primitive: 'plane', width: 4, height: 8}}
+                position={{x: 0, y: 2.5, z: -4}}
+                rotation={{x: 0, y: 0, z: 0}}
+                material={{shader: 'chromakey', src: '#wide1', color: '0 1 0'}}
+              />
+            }
+            {this.state.cameraTwoVideo === '#wide2' &&
+              <Entity
+                id={'wide2'}
+                name={'wide2'}
+                geometry={{primitive: 'plane', width: 4, height: 8}}
+                position={{x: 4, y: 2.5, z: -3}}
+                rotation={{x: 0, y: 0, z: 0}}
+                material={{shader: 'chromakey', src: '#wide2', color: '0 1 0'}}
+              />
+            }
+            {this.state.cameraTwoVideo === '#wide3' &&
+              <Entity
+                id={'wide3'}
+                name={'wide3'}
+                geometry={{primitive: 'plane', width: 4, height: 8}}
+                position={{x: 0, y: 2.5, z: -4}}
+                rotation={{x: 0, y: 0, z: 0}}
+                material={{shader: 'chromakey', src: '#wide3', color: '0 1 0'}}
+              />
+            }
+            {this.state.cameraTwoVideo === '#wide4' &&
+              <Entity
+                id={'wide4'}
+                name={'wide4'}
+                geometry={{primitive: 'plane', width: 4, height: 8}}
+                position={{x: 6, y: 5.5, z: -8}}
+                rotation={{x: 0, y: 0, z: 0}}
+                material={{shader: 'chromakey', src: '#wide4', color: '0 1 0'}}
+              />
+            }
           </Entity>
 
-          <Entity camera={{active: (this.state.currentCamera === 3)}} position={{x: -2, y: 0, z: -10}} />
-          <Entity camera={{active: (this.state.currentCamera === 4)}} position={{x: -2, y: 0, z: -13}} rotation={{x: 0, y: 0, z: -20}} />
           <Entity position={{x: -6, y: 2, z: -15}}>
+            <Entity camera={{active: (this.state.currentCamera === 3)}} position={{x: -1, y: -0.8, z: 3.8}} />
+            <Entity camera={{active: (this.state.currentCamera === 4)}} position={{x: 1, y: -2.1, z: 2.8}} rotation={{x: 0, y: 0, z: -40}} />
             <Entity
-              name={'axeboyswing1'}
-              geometry={{primitive: 'plane', width: 4, height: 8}}
-              position={{x: 0, y: 2.5, z: -4}}
-              rotation={{x: 0, y: 0, z: 0}}
-              material={{src: '#swing1'}}
-            />
-            <Entity name={'text'} position={{x: 0, y: 1, z: 0}} layout='type: line; plane: xz'>
+              layout='type: line; plane: xz'
+              name={'text'}
+              position={{x: -1, y: -2.2, z: 0.1}}
+              rotation={{x: 0, y: -15, z: -15}}
+              >
               <Entity
-                name={'text1'}
-                text-geometry={`value: ${this.state.text1}`}
-                material={'color: #fff'}
-              />
-              <Entity
-                name={'text2'}
-                rotation={{x: 0, y: 0, z: 0}}
-                text-geometry={`value: ${this.state.text2}`}
-                material={'color: #fff'}
+                text={{
+                  align: 'center',
+                  color: this.state.currentColor,
+                  font: this.state.currentFont,
+                  scale: {x: 4, y: 4, z: 4},
+                  shader: 'msdf',
+                  value: this.state.text1,
+                  width: this.state.currentFontSize
+                }}
               />
             </Entity>
             <Entity
-              name={'stump'}
-              geometry={{primitive: 'cylinder'}}
-              material={'color: crimson'}
-              height={1}
-              radius={1}
+              name={'swing1'}
+              geometry={{primitive: 'plane', width: 4, height: 8}}
               position={{x: 0, y: 0, z: 0}}
+              rotation={{x: 0, y: 0, z: 0}}
+              material={{shader: 'chromakey', src: '#swing1', color: '0 1 0'}}
             />
           </Entity>
 
